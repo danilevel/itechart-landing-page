e2560c81f765334d646d1e5af66cb13161327ddd
Danil <danil.cher57@gmail.com>
2022-10-15 15:50:07 +0300

    add: merge css files into one

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,6 +1,7 @@
 import dartSass from 'sass';
 import gulpSaas from 'gulp-sass';
 import rename from 'gulp-rename';
+import concatCss from "gulp-concat-css";
 
 const sass = gulpSaas(dartSass);
 
@@ -8,5 +9,6 @@ export const scss = () => {
     return app.gulp.src(app.path.src.scss, { sourcemaps: true })
         .pipe(sass({outputStyle: 'expanded'}))
         .pipe(rename({ extname: ".min.css"}))
+        .pipe(concatCss("style.min.css"))
         .pipe(app.gulp.dest(app.path.build.css));
 }
